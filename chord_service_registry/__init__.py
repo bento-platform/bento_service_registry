e51b2402e874aa7f8d19c9ada0e35c679ed07a52
David Lougheed <david.lougheed@gmail.com>
2019-11-04 12:56:15 -0500

    Use chord_lib method for getting version

--- chord_service_registry/__init__.py
+++ chord_service_registry/__init__.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python3
 
-from pkg_resources import get_distribution
+import os.path
+
+from chord_lib.utils import get_own_version
+from pathlib import Path
 
 name = "chord_service_registry"
-__version__ = get_distribution(name).version
+__version__ = get_own_version(os.path.join(Path(os.path.dirname(os.path.realpath(__file__))).parent, "setup.py"), name)
