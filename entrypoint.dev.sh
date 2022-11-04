d5e1046e65f7d9aaca6d2a850873092f62f206d9
David Lougheed <david.lougheed@gmail.com>
2022-11-04 10:20:02 -0400

    chore: properly set host and allow configurable internal port for entrypoints

--- entrypoint.dev.sh
+++ entrypoint.dev.sh
@@ -2,4 +2,10 @@
 
 export QUART_ENV=development
 export QUART_APP=bento_service_registry.app:application
-python -m debugpy --listen 0.0.0.0:5678 -m quart run
+
+if [ -z "${INTERNAL_PORT}" ]; then
+  # Set default internal port to 5000
+  INTERNAL_PORT=5000
+fi
+
+python -m debugpy --listen 0.0.0.0:5678 -m quart run --host 0.0.0.0 --port "${INTERNAL_PORT}"
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
+if [ -z "${INTERNAL_PORT}" ]; then
+  # Set default internal port to 5000
+  INTERNAL_PORT=5000
+fi
+
 uvicorn bento_service_registry.app:application \
   --workers 1 \
   --host 0.0.0.0 \
