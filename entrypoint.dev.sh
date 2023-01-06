dedb6e7107871772939ca0b7e8ac4f710ca7dac6
David Lougheed <david.lougheed@gmail.com>
2023-01-06 11:11:04 -0500

    export

--- entrypoint.dev.sh
+++ entrypoint.dev.sh
@@ -5,7 +5,7 @@ export QUART_APP=bento_service_registry.app:application
 
 if [ -z "${INTERNAL_PORT}" ]; then
   # Set default internal port to 5000
-  INTERNAL_PORT=5000
+  export INTERNAL_PORT=5000
 fi
 
 python -m poetry install
