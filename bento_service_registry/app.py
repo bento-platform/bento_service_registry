ed4b94f7e74f598e70a1b69832d34f0e680da069
David Lougheed <david.lougheed@gmail.com>
2023-01-13 10:34:04 -0500

    feat: validate ssl flag

--- bento_service_registry/app.py
+++ bento_service_registry/app.py
@@ -16,20 +16,26 @@ from werkzeug.exceptions import BadRequest, NotFound
 from .constants import SERVICE_TYPE, SERVICE_NAME
 from .routes import service_registry
 
+TRUTH_VALUES = ("true", "1")
+
 
 def get_bento_debug():
     # This is a function to allow monkey-patching the environment on app startup.
     return os.environ.get(
         "CHORD_DEBUG", os.environ.get("BENTO_DEBUG", os.environ.get("QUART_ENV", "production"))
-    ).strip().lower() in ("true", "1", "development")
+    ).strip().lower() in (*TRUTH_VALUES, "development")
 
 
 def create_app():
     app = Quart(__name__)
     bento_url = os.environ.get("CHORD_URL", os.environ.get("BENTO_URL", "http://0.0.0.0:5000/"))  # Own node's URL
+
+    bento_debug = get_bento_debug()
+    validate_ssl = os.environ.get("BENTO_VALIDATE_SSL", str(not bento_debug)).strip().lower() in TRUTH_VALUES
+
     app.config.from_mapping(
-        BENTO_DEBUG=get_bento_debug(),
-        BENTO_VALIDATE_SSL=not get_bento_debug(),
+        BENTO_DEBUG=bento_debug,
+        BENTO_VALIDATE_SSL=validate_ssl,
         BENTO_SERVICES=os.environ.get("CHORD_SERVICES", os.environ.get("BENTO_SERVICES", "chord_services.json")),
         BENTO_URL=bento_url,
         BENTO_PUBLIC_URL=os.environ.get("BENTO_PUBLIC_URL", bento_url),
