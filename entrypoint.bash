68dace94a58c385ab36babfb8da453bfb7dd1e1e
David Lougheed <david.lougheed@gmail.com>
2023-02-22 15:32:25 -0500

    fix: check env exists before trying to fix permissions

--- entrypoint.bash
+++ entrypoint.bash
@@ -6,7 +6,10 @@ cd /service-registry || exit
 source /create_service_user.bash
 
 # Fix permissions on /service-registry and /env
-chown -R bento_user:bento_user /service-registry /env
+chown -R bento_user:bento_user /service-registry
+if [[ -d /env ]]; then
+  chown -R bento_user:bento_user /env
+fi
 
 # Drop into bento_user from root and execute the CMD specified for the image
 exec gosu bento_user "$@"
