76ca69881be847640c43be09bd71e8d77951a69b
David Lougheed <david.lougheed@gmail.com>
2023-06-14 15:08:33 -0400

    test: fix tests for FastAPI

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,43 +1,31 @@
 import pytest
-from bento_service_registry.app import get_bento_debug
 
 # Cannot import anything from bento_service_registry here; has to be within
 # individual tests. Otherwise, we cannot configure the environment variables
 # to our liking for each test.
 
 
-def test_bento_debug_off(client):
-    assert not get_bento_debug()
-
-
-def test_bento_debug_on(client_debug_mode):
-    assert get_bento_debug()
-
-
-@pytest.mark.asyncio
-async def test_service_info(client):
-    r = await client.get("/service-info")
-    d = await r.get_json()
+def test_service_info(client):
+    r = client.get("/service-info")
+    d = r.json()
     # TODO: Check against service-info schema
     assert r.status_code == 200
     assert isinstance(d, dict)
     assert d["environment"] == "prod"
 
 
-@pytest.mark.asyncio
-async def test_service_info_debug_mode(client_debug_mode):
-    r = await client_debug_mode.get("/service-info")
-    d = await r.get_json()
+def test_service_info_debug_mode(client_debug_mode):
+    r = client_debug_mode.get("/service-info")
+    d = r.json()
     # TODO: Check against service-info schema
     assert r.status_code == 200
     assert isinstance(d, dict)
     assert d["environment"] == "dev"
 
 
-@pytest.mark.asyncio
-async def test_chord_service_list(client):
-    r = await client.get("/chord-services")
-    d = await r.get_json()
+def test_bento_service_list(client):
+    r = client.get("/bento-services")
+    d = r.json()
     assert r.status_code == 200
     assert len(d) == 1
     # TODO: Check against some schema
@@ -47,8 +35,8 @@ async def test_chord_service_list(client):
 async def test_service_list(client, service_info):
     service_info = await service_info
 
-    r = await client.get("/services")
-    d = await r.get_json()
+    r = client.get("/services")
+    d = r.json()
 
     assert r.status_code == 200
     assert len(d) == 1
@@ -59,13 +47,17 @@ async def test_service_list(client, service_info):
 async def test_service_detail(client, service_info):
     service_info = await service_info
 
-    r = await client.get(f"/services/{service_info['id']}")
-    d = await r.get_json()
+    r = client.get(f"/services/{service_info['id']}")
+    d = r.json()
+
+    print(f"/services/{service_info['id']}", d)
 
     assert r.status_code == 200
     assert d == service_info
 
-    r = await client.get("/services/does-not-exist")
+
+def test_service_detail_dne(client, service_info):
+    r = client.get("/services/does-not-exist")
     assert r.status_code == 404
 
 
@@ -73,8 +65,8 @@ async def test_service_detail(client, service_info):
 async def test_service_type_list(client, service_info):
     service_info = await service_info
 
-    r = await client.get("/services/types")
-    d = await r.get_json()
+    r = client.get("/services/types")
+    d = r.json()
 
     assert r.status_code == 200
     assert len(d) == 1
